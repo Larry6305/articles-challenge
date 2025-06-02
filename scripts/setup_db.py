b573cc328025e4d353115d138199552cea730f8c
Larry <melarry46@gmail.com>
2025-06-02 11:16:53 +0300

    created scripts to initialize db

---
+++ scripts/setup_db.py
@@ -0,0 +1,14 @@
+# scripts/setup_db.py
+import sqlite3
+from lib.db.connection import get_connection
+
+def setup_database():
+    conn = get_connection()
+    with open("lib/db/schema.sql") as f:
+        conn.executescript(f.read())
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    setup_database()
+    print("Database initialized.")
